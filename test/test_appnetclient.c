a404b209bc980b61d9536d4e7ce7a4fb5427f1c4
Tom Lee <github@tomlee.co>
2012-08-23 01:22:34 -0700

    Unit test for app_net_client_timeline

--- test/test_appnetclient.c
+++ test/test_appnetclient.c
@@ -78,6 +78,48 @@ _app_net_client_add_post (void)
     g_object_unref (client);
 }
 
+static void
+_app_net_client_timeline (void)
+{
+    AppNetClient *client;
+    AppNetStubHttpProvider *stub;
+    AppNetHttpResponse *resp;
+    AppNetHttpRequest *req;
+    gchar *body;
+    gsize bodylen;
+    AppNetPost *post;
+    GList *posts;
+    gchar *url;
+
+    make_app_net_client (&client, &stub);
+
+    if (!load_file ("appnetclient_timeline", &body, &bodylen)) {
+        g_test_fail ();
+        return;
+    }
+
+    resp = app_net_http_response_new (200, body, bodylen);
+    app_net_stub_http_provider_set_response (stub, resp);
+    posts = app_net_client_timeline (client);
+
+    req = app_net_stub_http_provider_get_request (stub);
+    url = g_strdup_printf ("%s/stream/0/posts/stream", base_url);
+    g_assert_cmpstr (app_net_http_request_get_url (req), ==, url);
+    g_free (url);
+
+    g_assert_cmpint (g_list_length (posts), ==, 1);
+
+    post = APP_NET_POST (g_list_first (posts)->data);
+    g_assert_cmpstr (app_net_http_request_get_method (req), ==, "GET");
+    g_assert_cmpstr (app_net_post_get_author (post), ==, "tomlee");
+    g_assert_cmpstr (app_net_post_get_author_name (post), ==, "Tom Lee");
+    g_assert_cmpstr (app_net_post_get_id (post), ==, "abcdefg");
+    g_assert_cmpstr (app_net_post_get_text (post), ==, "Hello, World");
+
+    g_object_unref (post);
+    g_object_unref (client);
+}
+
 int
 main (int argc, char **argv)
 {
@@ -86,6 +128,7 @@ main (int argc, char **argv)
 
     g_test_add_func ("/appnetclient/trivial", _app_net_client_trivial);
     g_test_add_func ("/appnetclient/add_post", _app_net_client_add_post);
+    g_test_add_func ("/appnetclient/timeline", _app_net_client_timeline);
 
     g_test_run ();
 
